3a4b425ffcc5c26a8339f821667d1c6924cd0aa0
Rajavel T <rajt1@publicisgroupe.net>
2023-09-15 14:37:50 +0530

    Changes to launch index file

--- commonjs.js
+++ commonjs.js
@@ -1,1 +1,107 @@
-console.log('Summa');
+window.digitalData = {
+    "page": {
+      "pageInfo": {
+        "name": "",
+        "pageType": "",
+        "siteSection": "",
+        "url": window.location.href ,
+        "fullPageUrl": window.location.href
+      },
+      "previousPageInfo": {
+        "name": "",
+        "pageType": "",
+        "siteSection": "",
+        "url": "",
+        "fullPageUrl": ""
+      }
+    },
+    "user": {
+      "customerIdentifierHash": "",
+      "loginStatus": "",
+      "profileID": "",
+      "userType": "Guest",
+      "deviceType": "Desktop"
+    }
+   };
+
+//Generic Functions
+window.genericFunctions = {
+    'cookieSetter' : function cookieSetter(name,value,days) {
+                        var expires = "";
+                        if (days) {
+                            var date = new Date();
+                            date.setTime(date.getTime() + (days*24*60*60*1000));
+                            expires = "; expires=" + date.toUTCString();
+                        }
+                        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+                    },
+    'cookieRemover' : function cookieRemover(name) {   
+                            document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+                    },
+    'cookieExtractor' : function cookieExtractor(name) {
+                            var nameEQ = name + "=";
+                            var ca = document.cookie.split(';');
+                            for(var i=0;i < ca.length;i++) {
+                                var c = ca[i];
+                                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+                            }
+                            return null;
+                        },
+    'pagePathLastValue' : function pagePathLastValue(){
+                            var urls = window.location.pathname.split('/');
+                            if(urls.length > 0){
+                              return urls[urls.length - 1];
+                            }
+                            else{
+                              return undefined;
+                            }
+                        }
+ 
+};
+
+
+//Global variables setup
+window.globalVariablesSetup = function globalVariablesSetup(){
+  //Page Name Setter - Dynamic
+  var lastValue = window.genericFunctions['pagePathLastValue']();
+  if(lastValue){
+    if(lastValue.indexOf('index') > -1){
+        //Home Page
+        window.digitalData.page.name = 'home';
+        window.digitalData.page.pageType = 'home';
+    }
+    else{
+        window.digitalData.page.name = lastValue.replace('.html','').replace('#','').trim();
+    }
+  }
+
+  //Previous Page details capturer
+  //Using Session Storage to get the information - PreviousPageInfo
+  var previousPageInfo = sessionStorage.getItem('previousPageInfo') ? JSON.parse(sessionStorage.getItem('previousPageInfo')) : '';
+  if(previousPageInfo){
+    //Already a page is viewed before
+    window.digitalData.previousPageInfo = previousPageInfo;
+    var pageInfo = window.digitalData.page ? window.digitalData.page : '';
+    //Setting up the previousPageInfo object with present page information which can be used in next page as previous page info
+    if(pageInfo){
+        sessionStorage.setItem('previousPageInfo',JSON.stringify(pageInfo));
+    }
+  }
+  else{
+    //landing page
+    var pageInfo = window.digitalData.page ? window.digitalData.page : '';
+    if(pageInfo){
+        sessionStorage.setItem('previousPageInfo',JSON.stringify(pageInfo));
+    }
+  }
+};
+
+//Calling the Variables Setup
+window.globalVariablesSetup();
+
+//Launch Injection
+var script = document.createElement('script');
+script.src = "https://assets.adobedtm.com/6d07e8369d1a/eddfb18269bf/launch-192d54330977-development.min.js";
+script.async = true;
+document.head.appendChild(script);
