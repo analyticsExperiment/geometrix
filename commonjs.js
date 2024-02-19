fc36fa69842f6bfc198261d07c8a1355fd595ae4
Rajavel T <rajt1@publicisgroupe.net>
2024-02-19 15:30:11 +0530

    New Changes

--- commonjs.js
+++ commonjs.js
@@ -13,8 +13,10 @@ window.digitalData = {
         "name": "",
         "pageType": "",
         "siteSection": "",
+        "siteSubsection" : "",
         "url": "",
-        "fullPageUrl": ""
+        "fullPageUrl": "",
+        "server" : "Geometrixx Clothiers Site"
       }
     },
     "user": {
@@ -122,4 +124,5 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NM6N9BRD');
 
-//Deep Analysis - Experiment
+//Global Components - Tracking
+
